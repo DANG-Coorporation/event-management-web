286490d4506d89cd3534a0e785ab9caddb870959
Galih Setyawan <galih.kamvax032@gmail.com>
2023-08-31 15:33:23 +0700

    add: route

---
+++ src/components/form/mobileNavbar.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchDeviceInfo } from "../../app/features/deviceDetection/deviceDetectionSlicer";
+
+export default function MobileNavbar() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchDeviceInfo());
+  }, [dispatch]);
+
+  const device = useSelector((state) => state.deviceDetection);
+  console.log(device);
+  return <></>;
+}
