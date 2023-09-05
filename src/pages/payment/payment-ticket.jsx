54a24bf48377199a0d2b0bd5890520a6f2062390
NaufanRikza <deathriver26@gmail.com>
2023-09-05 17:06:20 +0700

    feat : initial

---
+++ src/pages/payment/payment-ticket.jsx
@@ -0,0 +1,7 @@
+import { VStack } from "@chakra-ui/react";
+
+export default function PaymentTicketSelection() {
+  return <VStack>
+    
+  </VStack>;
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -7,6 +7,7 @@ import { Box } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import Discovery from "./pages/dicovery/discovery";
 import WebFooter from "./components/form/webFooter/webFooter";
+import PaymentTicketSelection from "./pages/payment/payment-ticket";
 export default function RouteList() {
   const isBlacken = useSelector((state) => state.screenDarken.isScreenDarken);
 
@@ -18,6 +19,7 @@ export default function RouteList() {
           <Route path="/" element={<Home />} />
           <Route path="/discovery" element={<Discovery />} />
           <Route path="*" element={<NotFound />} />
+          <Route path="/payment" element={<PaymentTicketSelection />} />
         </Routes>
         <Box
           className="app-darken"
