8e3aef70a43ae096fed657a4a2522eae903f31f1
NaufanRikza <deathriver26@gmail.com>
2023-09-07 22:47:12 +0700

    local change

--- src/routes.jsx
+++ src/routes.jsx
@@ -12,25 +12,27 @@ import WebFooter from "./components/form/webFooter/webFooter";
 import PaymentTicketSelection from "./pages/purchase/purchase-ticket";
 import LoginPage from "./pages/Login/logIn";
 import Attendancefirstpage from "./components/form/attendanceFirstPage/attendanceFirstPage";
-import { Tabstiket } from "./components/ui/tabsTicket/tabsTiket";
 export default function RouteList() {
   const isBlacken = useSelector((state) => state.screenDarken.isScreenDarken);
 
   return (
-    <Box className='app-container'>
+    <Box className="app-container">
       <WebNavbar />
-      <Box className='content-container'>
+      <Box className="content-container">
         <Routes>
-          <Route path='/create-event' element={<CreateEvent />} />
-          <Route path='/' element={<Home />} />
-          <Route path='/discovery' element={<Discovery />} />
-          <Route path="/attendancefirstpage" element={<Attendancefirstpage/>}/>
-          <Route path='*' element={<NotFound />} />
-          <Route path='/payment' element={<PaymentTicketSelection />} />
-          <Route path='/logIn' element={<LoginPage />} />
+          <Route path="/create-event" element={<CreateEvent />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/discovery" element={<Discovery />} />
+          <Route
+            path="/attendancefirstpage"
+            element={<Attendancefirstpage />}
+          />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/payment" element={<PaymentTicketSelection />} />
+          <Route path="/logIn" element={<LoginPage />} />
         </Routes>
         <Box
-          className='app-darken'
+          className="app-darken"
           display={isBlacken ? "block" : "none"}
           bg={isBlacken ? "rgba(0, 0, 0, 0.4)" : "transparent"}
         />
