05de526d6ab91f18c199b31daac2b818c7613126
NaufanRikza <deathriver26@gmail.com>
2023-09-08 13:19:21 +0700

    feat : temp

--- src/routes.jsx
+++ src/routes.jsx
@@ -12,25 +12,27 @@ import LoadingPage from "./pages/loading/loadingPage";
 import NotFound from "./pages/notfound";
 import PaymentTicketSelection from "./pages/purchase/purchase-ticket";
 import "./style.css";
+import RatingPage from "./pages/rating/ratingPage";
 export default function RouteList() {
   const isBlacken = useSelector((state) => state.screenDarken.isScreenDarken);
 
   return (
-    <Box className='app-container'>
+    <Box className="app-container">
       <WebNavbar />
-      <Box className='content-container'>
+      <Box className="content-container">
         <Routes>
-          <Route path='/create-event' element={<CreateEvent />} />
-          <Route path='/' element={<Home />} />
-          <Route path='/discovery' element={<Discovery />} />
-          <Route path='/event/:id' element={<Attendancefirstpage />} />
-          <Route path='*' element={<NotFound />} />
-          <Route path='/payment' element={<PaymentTicketSelection />} />
-          <Route path='/logIn' element={<LoginPage />} />
-          <Route path='/loading' element={<LoadingPage />} />
+          <Route path="/create-event" element={<CreateEvent />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/discovery" element={<Discovery />} />
+          <Route path="/event/:id" element={<Attendancefirstpage />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/payment" element={<PaymentTicketSelection />} />
+          <Route path="/logIn" element={<LoginPage />} />
+          <Route path="/loading" element={<LoadingPage />} />
+          <Route path="rating" element={<RatingPage />} />
         </Routes>
         <Box
-          className='app-darken'
+          className="app-darken"
           display={isBlacken ? "block" : "none"}
           bg={isBlacken ? "rgba(0, 0, 0, 0.4)" : "transparent"}
         />
